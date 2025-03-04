e548901c081fdaf6f50bce0eae848006096bf9d7
Nishchay Kumar Bhardwaj <168272378+nkbweb@users.noreply.github.com>
2025-03-04 17:25:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,27 @@
 import os
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-hard-to-guess-secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
     
-    # Postgresql Database Configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    # Database Configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://username:password@localhost/instagram_clon')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # Cloudinary configuration
-    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
-    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
-    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
+    # Cloudinary Configuration
+    CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL', '')
+    if CLOUDINARY_URL:
+        cloudinary_parsed = urlparse(CLOUDINARY_URL)
+        CLOUDINARY_CLOUD_NAME = cloudinary_parsed.hostname
+        CLOUDINARY_API_KEY = cloudinary_parsed.username
+        CLOUDINARY_API_SECRET = cloudinary_parsed.password
+    else:
+        CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME', 'diyijlhyt')
+        CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY', '169293457628317')
+        CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET', 'nuvzowumCQuL4voMZrf0loSBacc')
